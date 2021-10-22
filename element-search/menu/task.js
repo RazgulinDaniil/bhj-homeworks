c996ac982e22d7781e71a3ba74d631484d772170
Daniil <daniil.razgulin@mail.ru>
2021-10-22 22:28:04 +0300

    1.0v

---
+++ element-search/menu/task.js
@@ -0,0 +1,28 @@
+const menuSub = document.querySelectorAll(".menu_sub");
+
+
+for(let sub of menuSub) {
+    let arrNav = [];
+    arrNav.push(sub.closest(".menu__item"));
+    for(let key of arrNav) { 
+        let Links = [];
+        Links.push(key.firstElementChild);
+        Links.forEach(element => {
+            element.onclick = function () {
+                if(sub.classList.contains("menu_active")) {
+                    sub.classList.remove("menu_active");
+                    return false;
+                }
+                sub.classList.add("menu_active");
+                return false;
+            };
+
+            // element.addEventListener("click", () => {
+            //     event.preventDefault();
+            //     sub.classList.toggle("menu_active");
+                
+            // });
+        });
+    }
+}
+
