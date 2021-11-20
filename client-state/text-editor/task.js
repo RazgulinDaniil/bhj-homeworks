374103e2555a4d1eb85cd4cdb0ae86171f909438
Daniil <daniil.razgulin@mail.ru>
2021-11-21 00:58:36 +0300

    fixes

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,21 +1,14 @@
 const editor = document.querySelector('#editor');
 const cleaner = document.querySelector('#cleaner');
 
-editor.addEventListener('change', ()=> {
-    toLocalStorage('textarea',editor.value);
+editor.addEventListener('input', ()=> {
+    localStorage.setItem('textarea',editor.value);
 });
 cleaner.addEventListener('click', ()=> {
     editor.textContent = "";
     localStorage.clear();
 });
 
-if(getLocalStorage('textarea') != null) {
-    editor.textContent = getLocalStorage('textarea');
-}
-function toLocalStorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-function getLocalStorage(key) {
-    return JSON.parse(localStorage.getItem(key));
+if(localStorage.getItem('textarea')) {
+    editor.textContent = localStorage.getItem('textarea');
 }
